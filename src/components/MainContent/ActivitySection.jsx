9af1277acbabfa7d3dbfb44d39ed4beb9083fca6
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-06-03 14:05:32 +0100

    activitysection to continue

---
+++ src/components/MainContent/ActivitySection.jsx
@@ -0,0 +1,29 @@
+const ActivitySection = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card className="mb-4 shadow-sm">
+            <Card.Body>
+              <div className="d-flex justify-content-between align-items-start mb-3">
+                <Card.Title as="h2" className="h5 fw-bold mb-0">
+                  Attività
+                </Card.Title>
+
+                <a href="#" className="text-muted" aria-label="Modifica la sezione Attività">
+                  <Pencil size={20} />
+                </a>
+              </div>
+              <small className="mb-0">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste ad quas nam tenetur! Voluptate corporis accusamus quae expedita deserunt voluptas
+                id doloremque, minus mollitia ratione in dolore ut omnis nobis.
+              </small>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ActivitySection;
