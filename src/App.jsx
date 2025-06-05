dec8f17455e21b12dc31d2990d11724b7a0f91fc
Ren-Mor <renatomorelli@hotmail.it>
2025-06-05 10:38:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,10 @@ import MyNavBar from "./components/MyNavBar";
 import MySidebar from "./components/MySidebar";
 import ProfileHeader from "./components/ProfileHeader";
 import Footer from "./components/Footer";
-import AboutSection from "./components/MainContent/AboutSection";
-import ActivitySection from "./components/MainContent/ActivitySection";
-import AnalyticsSection from "./components/MainContent/AnalyticsSection";
 import { Col, Container, Row } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import MainContent from "./components/Homepage/MainContentHome";
 import RightSide from "./components/RightSide";
-import CreatePostCard from "./components/Homepage/CreatePostCard";
 import MainContentSections from "./components/MainContent/MainContentSections";
 import UserProfile from "./components/UserProfile";
 
