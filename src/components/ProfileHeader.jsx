161056b25886c8b40dbe35ea29f31a87a5da0cdb
DngDavide02 <dangelodavide02@gmail.com>
2025-06-06 16:04:44 +0200

    Update profile card background image URL

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -129,7 +129,7 @@ function MyProfileCard() {
           <div
             className="card-header-image"
             style={{
-              backgroundImage: `url('https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+              backgroundImage: `url('https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
             }}
           ></div>
 
